b7474901dd0ee5d5e979cc7e7a4ddb22f554877f
sangi424400 <sangi424400@gmail.com>
2026-05-11 13:06:09 +0530

    added task-management-applications files

---
+++ backend/models/Task.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    title: { type: String, required: true },
+    description: { type: String },
+    completed: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Task", taskSchema);
