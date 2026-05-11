3afbeaa9c0c30a5c9bd6eb55f69a6bb45b79be2c
sangi424400 <sangi424400@gmail.com>
2026-05-11 13:07:07 +0530

    added task-management-applications files

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+const protect = (req, res, next) => {
+  let token = req.headers.authorization;
+
+  if (token && token.startsWith("Bearer")) {
+    token = token.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "Invalid Token" });
+    }
+  } else {
+    return res.status(401).json({ message: "No Token Provided" });
+  }
+};
+
+export default protect;
